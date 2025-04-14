e8d64809afaa4dca5f4d96d262db923c189a972c
ivankvkharkiv <ivankvkharkiv@gmail.com>
2025-04-14 04:50:22 +0300

    simplified for more kiss principle

--- src/Service/WeatherService.php
+++ src/Service/WeatherService.php
@@ -2,9 +2,9 @@
 
 namespace App\Service;
 
-use App\Assembler\WeatherDataAssembler;
 use App\Client\WeatherClient;
 use App\Dto\WeatherResultDto;
+use App\Exception\WeatherArrayDataException;
 use Psr\Log\LoggerInterface;
 
 class WeatherService
@@ -16,10 +16,40 @@ class WeatherService
 
     public function getWeather(string $cityName): WeatherResultDto
     {
-        $weatherResultDto = WeatherDataAssembler::toDto($this->weatherClient->getWeather($cityName));
+        $weatherResultDto = $this->toDto($this->weatherClient->getWeather($cityName));
 
         $this->logger->emergency("Weather result for {$cityName}.", ['weatherData' => $weatherResultDto]);
 
         return $weatherResultDto;
+    }
+
+    private function toDto(array $weatherData)
+    {
+        if (!$this->weatherDataIsValid($weatherData)) {
+            throw new WeatherArrayDataException('Invalid weather data. Cannot create DTO');
+        }
+
+        return new WeatherResultDto(
+            $weatherData['location']['name'],
+            $weatherData['location']['country'],
+            $weatherData['current']['temp_c'],
+            $weatherData['current']['condition']['text'],
+            $weatherData['current']['humidity'],
+            $weatherData['current']['wind_kph'],
+            new \DateTime($weatherData['current']['last_updated']),
+        );
+    }
+
+    private function weatherDataIsValid(array $weatherData): bool
+    {
+        if (
+            2 !== count(array_intersect(['location', 'current'], array_keys($weatherData)))
+            || 2 !== count(array_intersect(['name', 'country'], array_keys($weatherData['location'])))
+            || 5 !== count(array_intersect(['temp_c', 'condition', 'humidity', 'wind_kph', 'last_updated'], array_keys($weatherData['current'])))
+        ) {
+            return false;
+        }
+
+        return true;
     }
 }
